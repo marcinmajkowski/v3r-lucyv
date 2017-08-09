51076cf135c194290b23b97d2781ae26037de9e8
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-08-09 22:31:10 +0200

    Start naively showing some lyrics

--- animation.js
+++ animation.js
@@ -14,9 +14,12 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     playerMsElapsed(player)
-        .subscribe(t => {
-            console.log(msToTime(t));
-            content.innerHTML = msToTime(t);
+        .distinctUntilChanged()
+        .do(console.log)
+        .map(textForPlayerMsElapsed)
+        .distinctUntilChanged()
+        .subscribe(text => {
+            content.innerHTML = text;
         });
 }
 
@@ -53,4 +56,21 @@ function msToTime(s) {
 function zeroPad(n, width) {
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+
+const lyrics = [
+    {startMs: 20987, endMs: 22646, text: 'Nie jestem demonem a...'},
+    {startMs: 22646, endMs: 24474, text: 'potrafię opętać słowem.'},
+    {startMs: 24474, endMs: 27322, text: 'Otworzę głowy nim na zdrowie powie śmierć wam.'},
+    {startMs: 27322, endMs: 30537, text: 'Nie pochodzę z piekła i choć mowę mam z piekła rodem,'},
+    {startMs: 30537, endMs: 33808, text: 'ono nie jest wrogiem. To dla mnie konkurencja.'},
+    {startMs: 33808, endMs: 36668, text: 'Zmienia się świat boy, ziemia to bagno i nie ma to tamto'},
+    {startMs: 36668, endMs: 39852, text: 'Jak trzeba się nie bać to scena jest niema i nie ma co jebać się z prawdą'},
+    {startMs: 39852, endMs: 41292, text: 'Schemat zjebał się dawno, marzenia zamnienia na banknot'},
+    {startMs: 41292, endMs: 99999, text: 'Mówi że trzeba doceniać co się ma i nie ma znaczenia kto zajebał światło dla nich'}
+];
+
+function textForPlayerMsElapsed(ms) {
+    const entry = lyrics.find(entry => ms <= entry.endMs && ms >= entry.startMs);
+    return entry ? entry.text : '';
 }
