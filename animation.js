f3044278d05e130d4ad2dd8cc0385868c7305734
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-08-09 20:29:25 +0200

    Move js code to separate file

---
+++ animation.js
@@ -0,0 +1,23 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    console.log('player ready');
+}
+
+function onPlayerStateChange(event) {
+    console.log(event.data);
+    console.log(event.target.getCurrentTime());
+}
